f358376bc94cef34ed7adc3524be4329687486bb
Alessandro <luisialessandro95@gmail.com>
2021-11-29 17:02:12 +0100

    css: input tex e lista

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,8 @@
-// alert('Ciaone');
+// Descrizione:
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
 // MILESTONE 1
 // Stampare all'interno di una lista, un item per ogni todo.
